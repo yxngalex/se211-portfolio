0f52c95ff5da0e1b4bfa8d7186a47220654b3bb0
Aleksa <aleksa.cekic8@gmail.com>
2021-05-12 12:35:33 +0200

    finished stylizing header, added router to the application and some animations

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.scss';
 import NavBar from "./components/navbar/navbar.component";
+import {Switch,  Route} from "react-router-dom";
 
 function App() {
   return (
     <div>
       <NavBar/>
+      <Switch>
+          <Route exact path='/'/>
+      </Switch>
     </div>
   );
 }
---
+++ src/components/navbar/navbar.component.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import './navbar.styles.scss';
+import '../../assets/common/common.scss';
+
+import {Link} from "react-router-dom";
+
+const NavBar = () => {
+    return (
+        <div className='header'>
+            <Link to='/' className='logo scale-in-center'>
+                Aleksa Cekić
+            </Link>
+            <div className='options'>
+                <Link className='option scale-in-center' to='/test'>TEST</Link>
+                <Link className='option scale-in-center' to='/contact'>CONTACT</Link>
+            </div>
+        </div>
+    );
+}
+
+export default NavBar;
