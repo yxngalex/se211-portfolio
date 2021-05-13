754e119ed2bd9b85fcc28fbaf75dfc314a4b15ef
Aleksa <aleksa.cekic8@gmail.com>
2021-05-13 23:43:10 +0200

    added links

--- src/components/about/about.component.jsx
+++ src/components/about/about.component.jsx
@@ -9,16 +9,24 @@ const About = () => {
             <h2>ABOUT</h2>
             <div className="icons">
                 <Tooltip title="Email" arrow>
+                    <a rel="noreferrer" href="mailto://aleksa.cekic8@gmail.com" target="_blank">
                     <i className="fa fa-at fa-3x" aria-hidden="true"/>
+                    </a>
                 </Tooltip>
                 <Tooltip title="Github" arrow>
-                    <i className="fa fa-github fa-3x"/>
+                    <a rel="noreferrer" href="https://github.com/yxngalex" target="_blank">
+                        <i className="fa fa-github fa-3x"/>
+                    </a>
                 </Tooltip>
                 <Tooltip title="Facebook" arrow>
-                    <i className="fa fa-facebook-square fa-3x" aria-hidden="true"/>
+                    <a rel="noreferrer" href="https://www.facebook.com/aestheticsgodd/" target="_blank">
+                        <i className="fa fa-facebook-square fa-3x" aria-hidden="true"/>
+                    </a>
                 </Tooltip>
                 <Tooltip title="Instagram" arrow>
-                    <i className="fa fa-instagram fa-3x" aria-hidden="true"/>
+                    <a rel="noreferrer" href="https://www.instagram.com/yxngvlex/" target="_blank">
+                        <i className="fa fa-instagram fa-3x" aria-hidden="true"/>
+                    </a>
                 </Tooltip>
             </div>
         </div>
