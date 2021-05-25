ec1369015bf21e62723341b33f8f4645a716e0c5
Aleksa <aleksa.cekic8@gmail.com>
2021-05-25 22:57:11 +0200

    added snackbar to contact-form

--- src/components/contact-form/contact-form.component.jsx
+++ src/components/contact-form/contact-form.component.jsx
@@ -1,10 +1,10 @@
 import React, {useState} from "react";
-import {Button, TextField, Tooltip} from "@material-ui/core";
+import {TextField, Tooltip, Snackbar, Button, IconButton} from "@material-ui/core";
+import CloseIcon from '@material-ui/icons/Close';
 import {v4 as uuidv4} from "uuid";
-
-import "./contact-form.styles.scss";
 import {saveContact} from "../../services/services.service";
 
+import "./contact-form.styles.scss";
 
 const ContactForm = () => {
     const [contact, setContact] = useState({
@@ -14,17 +14,21 @@ const ContactForm = () => {
         email: "",
         budget: ""
     });
+    const [open, setOpen] = useState(false);
+    const [disabled, setDisabled] = useState(true);
 
-    const handleInputChange = (event) => {
-        setContact({...contact, [event.target.id]: event.target.value});
+    const handleInputChange = (ev) => {
+        setContact({...contact, [ev.target.id]: ev.target.value});
+        if (contact) {
+            setDisabled(false);
+        }
     }
 
     const handleSubmit = (ev) => {
         ev.preventDefault();
         if (contact.name.trim() && contact.lastName.trim() && contact.email.trim() && contact.budget.trim()) {
             saveContact({...contact, id: uuidv4()})
-                .then(res => {
-                    console.log(res);
+                .then(() => {
                     setContact({
                         id: "",
                         name: "",
@@ -36,59 +40,94 @@ const ContactForm = () => {
         }
     }
 
+
+    const handleClick = () => {
+        setOpen(true);
+    };
+
+    const handleClose = (event, reason) => {
+        if (reason === "clickaway") {
+            return;
+        }
+        setOpen(false);
+    };
+
     return (
-        <div className="contact-form col-md-9 mb-md-0 mb-5">
-            <form onSubmit={handleSubmit}>
-                <div className="row">
-                    <div className="mb-md-4">
-                        <TextField
-                            id="name"
-                            className="text-field"
-                            type="text"
-                            label="Your name"
-                            value={contact?.name}
-                            onChange={handleInputChange}
-                            required
-                        />
-                        <TextField
-                            id="lastName"
-                            className="text-field"
-                            type="text"
-                            label="Your last name"
-                            value={contact?.lastName}
-                            onChange={handleInputChange}
-                            required
+        <div>
+            <div className="contact-form col-md-9 mb-md-0 mb-5">
+                <form onSubmit={handleSubmit}>
+                    <div className="row">
+                        <div className="mb-md-4">
+                            <TextField
+                                id="name"
+                                className="text-field"
+                                type="text"
+                                label="Your name"
+                                value={contact?.name}
+                                onChange={handleInputChange}
+                                required
+                            />
+                            <TextField
+                                id="lastName"
+                                className="text-field"
+                                type="text"
+                                label="Your last name"
+                                value={contact?.lastName}
+                                onChange={handleInputChange}
+                                required
+                            />
+                        </div>
+                        <div className="mb-md-4">
+                            <TextField
+                                id="email"
+                                className="text-field"
+                                type="email"
+                                label="Your email"
+                                value={contact?.email}
+                                onChange={handleInputChange}
+                                required
+                            />
+                        </div>
+                        <div className="mb-md-5">
+                            <TextField
+                                id="budget"
+                                className="text-field"
+                                type="number"
+                                label="Your budget"
+                                value={contact?.budget}
+                                onChange={handleInputChange}
+                                required
+                            />
+                        </div>
+                        <div>
+                            <Tooltip title="Submit" arrow>
+                                <input type="submit" className="btn btn-submit" value="Submit" onSubmit={handleClick}
+                                       disabled={disabled}/>
+                            </Tooltip>
+                        </div>
+                        <Snackbar
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'left',
+                            }}
+                            open={open}
+                            autoHideDuration={6000}
+                            onClose={handleClose}
+                            message="Note archived"
+                            action={
+                                <React.Fragment>
+                                    <Button color="secondary" size="small" onClick={handleClose}>
+                                        UNDO
+                                    </Button>
+                                    <IconButton size="small" aria-label="close" color="inherit" onClick={handleClose}>
+                                        <CloseIcon fontSize="small"/>
+                                    </IconButton>
+                                </React.Fragment>
+                            }
                         />
                     </div>
-                    <div className="mb-md-4">
-                        <TextField
-                            id="email"
-                            className="text-field"
-                            type="email"
-                            label="Your email"
-                            value={contact?.email}
-                            onChange={handleInputChange}
-                            required
-                        />
-                    </div>
-                    <div className="mb-md-5">
-                        <TextField
-                            id="budget"
-                            className="text-field"
-                            type="number"
-                            label="Your budget"
-                            value={contact?.budget}
-                            onChange={handleInputChange}
-                            required
-                        />
-                    </div>
-                    <div>
-                        <Tooltip title="Submit" arrow>
-                            <input type="submit" value="submit" />
-                        </Tooltip>
-                    </div>
-                </div>
-            </form>
+                </form>
+            </div>
         </div>
     );
 }
--- src/pages/workpage/workpage.component.jsx
+++ src/pages/workpage/workpage.component.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import Showcase from "../../components/showcase/showcase.component";
-import img from "../../assets/images/gta-online.jpg";
 
 import "./workpage.styles.scss";
 
